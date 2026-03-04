2f04a9074f34863cec33497b89fd93f53957e0f2
Schei/εγώ <dscr9669@gmail.com>
2026-03-04 14:05:01 -0300

    Update Problema_Macaco.pl

--- Problema_Macaco_Prolog/scr/Problema_Macaco.pl
+++ Problema_Macaco_Prolog/scr/Problema_Macaco.pl
@@ -1,13 +1,11 @@
 movimento(estado(centro, nacadeira, centro, naoter), agarrar, estado(centro, nacadeira, centro, simter), [agarrar]).
-
 movimento(estado(Posicao, chao, Posicao, Banana), subir, estado(Posicao, nacadeira, Posicao, Banana), [subir]).
-
 movimento(estado(PosicaoA, chao, PosicaoA, Banana), empurrar(PosicaoA, PosicaoB), estado(PosicaoB, chao, PosicaoB, Banana), [empurrar(PosicaoA, PosicaoB)]).
-
 movimento(estado(PosicaoA, chao, Cadeira, Banana), andar(PosicaoA, PosicaoB), estado(PosicaoB, chao, Cadeira, Banana), [andar(PosicaoA, PosicaoB)]).
 
 pode_pegar(estado(_, _, _, simter), []).
-pode_pegar(Estado, Sequencia) :-
-    movimento(Estado, _, NovoEstado, Lista),
-    pode_pegar(NovoEstado, NovaSequencia),
-    append(Lista, NovaSequencia, Sequencia).
+
+pode_pegar(Estado, Sequencia) :- 
+movimento(Estado, _,NovoEstado, ListaAcao), 
+pode_pegar(NovoEstado, SubSequencia), 
+append(ListaAcao, SubSequencia, Sequencia).
