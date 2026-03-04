886eab714c764990700ba432afcc656d97237c72
Schei/εγώ <dscr9669@gmail.com>
2026-03-04 13:17:24 -0300

    Refactor movimento predicates to include Banana state
    Atualização

--- Problema_Macaco_Prolog/scr/Problema_Macaco.pl
+++ Problema_Macaco_Prolog/scr/Problema_Macaco.pl
@@ -1,10 +1,10 @@
-movimento(estado(Posicao, nacadeira, Posicao, naoter), agarrar, estado(Posicao, nacadeira, Posicao, simter), [agarrar]).
+movimento(estado(centro, nacadeira, centro, naoter), agarrar, estado(centro, nacadeira, centro, simter), [agarrar]).
 
-movimento(estado(Posicao, chao, Posicao, naoter), subir, estado(Posicao, nacadeira, Posicao, naoter), [subir]).
+movimento(estado(Posicao, chao, Posicao, Banana), subir, estado(Posicao, nacadeira, Posicao, Banana), [subir]).
 
-movimento(estado(PosicaoA, chao, PosicaoA, Ter), empurrar(PosicaoA, PosicaoB), estado(PosicaoB, chao, PosicaoB, Ter), [empurrar(PosicaoA, PosicaoB)]).
+movimento(estado(PosicaoA, chao, PosicaoA, Banana), empurrar(PosicaoA, PosicaoB), estado(PosicaoB, chao, PosicaoB, Banana), [empurrar(PosicaoA, PosicaoB)]).
 
-movimento(estado(PosicaoA, chao, Cadeira, Ter), andar(PosicaoA, PosicaoB), estado(PosicaoB, chao, Cadeira, Ter), [andar(PosicaoA, PosicaoB)]).
+movimento(estado(PosicaoA, chao, Cadeira, Banana), andar(PosicaoA, PosicaoB), estado(PosicaoB, chao, Cadeira, Banana), [andar(PosicaoA, PosicaoB)]).
 
 pode_pegar(estado(_, _, _, simter), []).
 pode_pegar(Estado, Sequencia) :-
